ce35f61b9114215d1ddb6d0f6bf476af4139354c
DynamicDesarrollo <sirjhan@gmail.com>
2025-08-09 17:17:39 -0500

    Configuración DB

--- votantes-backend/src/utils/db.js
+++ votantes-backend/src/utils/db.js
@@ -5,20 +5,18 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool(
-  process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
-      }
-    : {
-        user: process.env.PGUSER,
-        host: process.env.PGHOST,
-        database: process.env.PGDATABASE,
-        password: process.env.PGPASSWORD,
-        port: process.env.PGPORT
-      }
-);
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    })
+  : new Pool({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT
+    });
 
 const db = {
   query: (text, params) => pool.query(text, params),
