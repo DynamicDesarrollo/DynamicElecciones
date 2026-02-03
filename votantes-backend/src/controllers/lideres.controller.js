344b091eb6f3d04950063964670d8a66047564cf
Sirjhan Betancourt <sirjhan@gmail.com>
2026-02-02 20:37:24 -0500

    Debug: log bytes del id recibido en DELETE líder

--- votantes-backend/src/controllers/lideres.controller.js
+++ votantes-backend/src/controllers/lideres.controller.js
@@ -97,6 +97,7 @@ const updateLider = async (req, res) => {
   
 
 const deleteLider = async (req, res) => {
+      console.log('ID recibido (bytes):', Buffer.from(id).toString('hex'));
     // Log de conexión a la base de datos
     console.log('DB HOST:', process.env.PGHOST, 'DB NAME:', process.env.PGDATABASE, 'DB USER:', process.env.PGUSER);
   let { id } = req.params;
