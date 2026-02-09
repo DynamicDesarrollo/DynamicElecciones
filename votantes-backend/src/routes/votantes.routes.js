23c09f8bcfd2e3567d4004784c3998bc3a3504f2
Sirjhan <sirjhan@gmail.com>
2026-02-09 17:07:03 -0500

    fix(rutas): /filtrar usa getVotantes para exponer direccion_lider y logs

--- votantes-backend/src/routes/votantes.routes.js
+++ votantes-backend/src/routes/votantes.routes.js
@@ -8,7 +8,7 @@ const {
   getTotalVotantes,
   validarCedula
 } = require('../controllers/votantes.controller');
-const { filtrarVotantes } = require('../controllers/reportes.controller');
+// const { filtrarVotantes } = require('../controllers/reportes.controller');
 const { verificarToken } = require('../middlewares/auth');
 
 const router = express.Router();
@@ -18,7 +18,7 @@ router.post('/', createVotante);
 router.put('/:id', updateVotante);
 router.delete('/:id', deleteVotante);
 router.get('/votantes', verificarToken, getVotantes);
-router.get('/filtrar', filtrarVotantes);
+router.get('/filtrar', getVotantes);
 router.get('/total', getTotalVotantes);
 router.get('/validar-cedula/:cedula', validarCedula);
 module.exports = router;
